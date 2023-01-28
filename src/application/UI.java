8f5f5bdaf886269792443df5d6897bbabab8119e
Caio Souza <caio.cesarads@gmail.com>
2023-01-28 12:14:41 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -60,6 +58,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
